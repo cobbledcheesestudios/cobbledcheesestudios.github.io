51bf52f38eecc621fb14d44380378afeada60ade
cobbledcheesestudios <cobbledcheesestudios@kittymail.com>
2024-08-04 14:39:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function discord() {
-  window.open("https://discord.gg/F32scgr2nc", "test")
+  window.open("https://discord.gg/F32scgr2nc")
+}
+function roblox() {
+  window.open("https://www.roblox.com/groups/33318243/cobbled-cheese-studios#!/about")
 }
